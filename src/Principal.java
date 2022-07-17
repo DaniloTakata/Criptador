9a0f6a56c500f6137902392c9d5f92538bd04f08
DaniloTakata <daniloissamu@gmail.com>
2022-07-17 11:33:13 -0300

    Refatoracao total do codigo, existe erro na descriptografia

--- src/Principal.java
+++ src/Principal.java
@@ -1,13 +1,38 @@
 import criptadores.CriptoDES;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 public class Principal {
-    public static void main(String [] args) throws NoSuchAlgorithmException {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Texto: ");
+        String mensagem = scan.next();
 
+        try {
+            Key chave = CriptoDES.keyDES();
+            System.out.println("Chave: " + chave);
 
+            String criptografada = CriptoDES.criptogaphy(mensagem, chave);
+            String descriptagrafada = CriptoDES.descriptogaphy(criptografada, chave);
+
+            System.out.println("Criptografada: " + criptografada);
+            System.out.println("Descriptofada: " + descriptagrafada);
+        } catch (NoSuchPaddingException | IllegalBlockSizeException | NoSuchAlgorithmException | BadPaddingException |
+                 InvalidKeyException e) {
+            System.out.println("Unable to do de criptography.");
+        }
+
+        scan.close();
+        System.out.println("System has ended.");
     }
 }
+/*
+* com.sun.crypto.provider.DESKey@18466
+* ?skY??*/
--- src/criptadores/CriptoDES.java
+++ src/criptadores/CriptoDES.java
@@ -4,84 +4,26 @@ import javax.crypto.*;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class CriptoDES {
 
-    //Criando a cifra de criptografia
-    private static Cipher cifraDES;
+  public static Key keyDES() throws NoSuchAlgorithmException {
+      KeyGenerator keyGenerator = KeyGenerator.getInstance("DES");
+      return keyGenerator.generateKey();
+  }
 
-    //Gerador de chave de Criptografia simétrica
-    private static KeyGenerator keyGenerator;
+  public static String criptogaphy(String message, Key key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+      Cipher cipherDES = Cipher.getInstance("DES/ECB/PKCS5Padding");
+      cipherDES.init(Cipher.ENCRYPT_MODE, key);
+      byte[] secret_message = cipherDES.doFinal(message.getBytes());
+      return new String(secret_message);
+  }
 
-    //Criando a chave de Criptografia simétrica
-    private static SecretKey chaveDES;
-
-    public static Object chave_cripEdes() {
-        try {
-            //Criador de chave de criptografia, utilizando o algoritmo DES
-            keyGenerator = KeyGenerator.getInstance("DES");
-
-            //Ativando a chave de criptografia
-            chaveDES = keyGenerator.generateKey();
-            return chaveDES;
-
-        } catch (NoSuchAlgorithmException e) {
-            return e.getMessage();
-        }
+    public static String descriptogaphy(String secret_message, Key key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipherDES = Cipher.getInstance("DES/ECB/PKCS5Padding");
+        cipherDES.init(Cipher.DECRYPT_MODE, key);
+        byte[] message = cipherDES.doFinal(secret_message.getBytes());
+        return new String(message);
     }
 
-    public static Object criptografia(byte[] mensagem) {
-        try {
-            //Utiliza a chave gerada
-            Object chave_criptografia = chave_cripEdes();
-
-            //Método de transformação da cifra em questão
-            cifraDES = Cipher.getInstance("DES/CBC/PKCS5Padding");
-
-            //Inicializa o método de criptografia
-            cifraDES.init(Cipher.ENCRYPT_MODE, (Key) chave_criptografia);
-
-            //A criptação em andamento
-            byte[] texto_encriptografado = cifraDES.doFinal(mensagem);
-            return texto_encriptografado;
-
-        }catch(NoSuchAlgorithmException e){
-            return "Erro no algoritmo de descriptografia.";
-        }catch(NoSuchPaddingException e){
-            return "Padding não encontrado.";
-        }catch(InvalidKeyException e){
-            return "Chave de descriptografia inválida.";
-        }catch(IllegalBlockSizeException e){
-            return "Tamanho de bloco ilegal.";
-        }catch(BadPaddingException e){
-            return "Erro no padding.";
-        }
-    }
-
-    public static Object descriptografia(byte[] mensagem, Key chave) {
-        try {
-            //Método de transformação da cifra em questão
-            cifraDES = Cipher.getInstance("DES/CBC/PKCS5Padding");
-
-            //Inicializa o método de descriptografia
-            cifraDES.init(Cipher.DECRYPT_MODE, chave);
-
-            //A criptação em andamento
-            byte[] texto_descriptografado = cifraDES.doFinal(mensagem);
-            return texto_descriptografado;
-
-        }catch(NoSuchAlgorithmException e){
-            return "Erro no algoritmo de descriptografia.";
-        }catch(NoSuchPaddingException e){
-            return "Padding não encontrado.";
-        }catch(InvalidKeyException e){
-            return "Chave de descriptografia inválida.";
-        }catch(IllegalBlockSizeException e){
-            return "Tamanho de bloco ilegal.";
-        }catch(BadPaddingException e){
-            return "Erro no padding.";
-        }
-    }
 }
